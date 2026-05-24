32abf22969081a7c4a97733cf6d1b88b9e0c9d55
Anton Krivoborodov <63401640+antonkri@users.noreply.github.com>
2026-05-24 11:15:33 +0000

    refactor: scrample app

--- scorex/internal/model/module.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type ModuleInfo struct {
-	Version string `json:"version"`
-	Hash    string `json:"hash"`
-	Repo    string `json:"repo"`
-	Branch  string `json:"branch,omitempty"`
-}
--- scorex/internal/utils/skeleton_properties.go
+++
@@ -1,6 +0,0 @@
-package utils
-
-import "scorex/internal/service/skeleton"
-
-// Deprecated: use scorex/internal/service/skeleton.Properties.
-type SkeletonProperties = skeleton.Properties
